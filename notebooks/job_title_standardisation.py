4d3b1f5d60711a683bfd90984db0207166f46b8c
Igor <84167672+lethalwombat@users.noreply.github.com>
2023-03-14 13:30:59 +1000

    script added

---
+++ notebooks/job_title_standardisation.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Import libraries
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# Import data
+
+# In[2]:
+
+
+# job titles from rocket
+df_test_data = (
+    pd.read_csv('job_titles_rocket.gz', delimiter='\t', encoding = "ISO-8859-1", names=['job_title_raw', 'n'])
+#     pd.read_csv('job_titles_rocket.gz', delimiter='\t', names=['job_title', 'n'])    
+    .dropna()
+    .reset_index(drop=True)
+)
+
+# transform the data
+df_test_data = (
+    df_test_data
+    .assign(
+        job_title_raw=df_test_data['job_title_raw'].str.lower().str.strip()
+    )
+)
+
+# import assigned roles from thesaurus
+df_assigned_roles = (
+    pd.read_csv('job_title_dictionary.txt', delimiter='\t')
+    .query('FindPhraseStatus == "assignedrole"')
+    .drop(columns=['FindPhraseStatus'])
+    .reset_index(drop=True)
+)
+
+# transform role data
+df_assigned_roles = (
+    df_assigned_roles
+    .assign(
+        job_title_raw=df_assigned_roles['FindPhrase'].str.lower().str.strip(),
+        job_title=df_assigned_roles['ReplacePhrase'].str.lower().str.strip()
+    )
+    [['job_title_raw', 'job_title']]
+)
+
+
+# Find out exact matches
+
+# In[3]:
+
+
+# first merge
+df_exact_matches = (
+    df_test_data
+    .merge(df_assigned_roles, how='outer', left_on='job_title_raw', right_on='job_title_raw', indicator=True)
+)
+
+# split data into matches and non-matches
+df_exact_non_matches = (
+    df_exact_matches
+    .query('_merge == "left_only"')
+    .drop(columns=['_merge', 'job_title'])
+    .reset_index(drop=True)
+)
+df_exact_matches = (
+    df_exact_matches
+    .query('_merge == "both"')
+    .drop(columns=['_merge'])
+    .reset_index(drop=True)    
+)
+
+# display stats
+print('Total records: {}'.format(df_test_data.shape[0]))
+print('Matched: {}'.format(df_exact_matches.shape[0]))
+print('Unmatched: {}'.format(df_exact_non_matches.shape[0]))
+
+
+# Define matching function
+
+# In[ ]:
+
+
+get_ipython().run_cell_magic('time', '', "from rapidfuzz import process\nfrom pandarallel import pandarallel\npandarallel.initialize(progress_bar=True, nb_workers=4)\n\ndef match_job_titles(df, dictionary=None, score_cutoff=90):\n    # nothing to look up\n    if dictionary is None:\n        print('No dictionary set!')\n        return df\n    \n    # get roles to look up \n    _roles = dictionary['job_title_raw'].unique()\n\n    # matching function\n    _match_func = lambda x : process.extractOne(x, _roles)\n\n    # apply matching function\n    df = (\n        df\n        .assign(\n            _match=df['job_title_raw'].parallel_apply(_match_func)\n        )\n    )\n    # filter out bad quality matches\n    df = (\n        df\n        .assign(\n            job_title_matched=df['_match'].apply(lambda x : x[0]),            \n            _score=df['_match'].apply(lambda x : x[1]),\n        )\n        .query('_score >= @score_cutoff')\n        .drop(columns=['_match', '_score'])\n        .reset_index(drop=True)\n    )\n    return df\n\n# apply fuzzy matching and match back to the dictionary\ndf_fuzzy_matches = (\n    df_exact_non_matches\n#     .head(10000) # testing purposes\n    .pipe(match_job_titles, df_assigned_roles)\n    [['job_title_matched', 'n']]\n    .merge(df_assigned_roles, how='left', left_on='job_title_matched', right_on='job_title_raw')\n    .drop(columns=['job_title_matched'])\n    [['job_title_raw', 'n', 'job_title']]    \n)\n\n# display stats\nprint('Total records: {}'.format(df_test_data.shape[0]))\nprint('Exact matches: {}'.format(df_exact_matches.shape[0]))\nprint('Fuzzy matches: {}'.format(df_fuzzy_matches.shape[0]))\n")
+
+
+# Combine results
+
+# In[ ]:
+
+
+df_result = (
+    pd.concat([df_fuzzy_matches, df_exact_matches], ignore_index=True)
+)
+
+# display stats
+print('Match ratio : {:.3f}'.format(df_result['n'].sum() / df_test_data['n'].sum()))
+print('Categories: {:,} -> {:,}'.format(df_result['job_title_raw'].nunique(), df_result['job_title'].nunique()))
+
+# 10 most transformed job titles 
+(
+    df_result
+    .groupby('job_title')
+    .size()
+    .sort_values(ascending=False)
+    .head(10)
+)
+
